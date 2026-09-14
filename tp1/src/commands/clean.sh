971d69fec7b6192255e5ef091849e559aedf783e
14g0 <iagocesarts@gmail.com>
2026-09-14 12:07:33 -0300

    feat: adiciona o comando clean no projeto

--- tp1/src/commands/clean.sh
+++ tp1/src/commands/clean.sh
@@ -1,1 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+limparProjeto() {
+    mensagemComando "-Limpando projeto..." aviso
+    mensagemVerbose "Removendo arquivos temporários gerados pela ferramenta" aviso
+    mensagemDebug "rm -rf "$CBUILD_TARGET_DIR/build""
+
+    if ! rm -rf "$CBUILD_TARGET_DIR/build"; then
+        saidaDeErro "Falha ao remover diretório de build"
+    fi
+
+    mensagemVerbose "Projeto limpo com sucesso" sucesso
+    mensagemComando "Projeto limpo com sucesso" sucesso 1
+}
+
+limparProjeto
+return 0
