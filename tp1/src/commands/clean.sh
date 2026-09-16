7e7eaf62cc9de1008f92f9aee5866dd498edc876
14g0 <iagocesarts@gmail.com>
2026-09-16 16:57:31 -0300

    fix: melhora execução do comando clean

--- tp1/src/commands/clean.sh
+++ tp1/src/commands/clean.sh
@@ -2,14 +2,25 @@
 
 limparProjeto() {
     mensagemComando "-Limpando projeto..." aviso
-    mensagemVerbose "Removendo arquivos temporários gerados pela ferramenta" aviso
-    mensagemDebug "rm -rf "$CBUILD_TARGET_DIR/build""
 
-    if ! rm -rf "$CBUILD_TARGET_DIR/build"; then
-        saidaDeErro 501 "Falha ao remover diretório de build"
-    fi
+    mensagemVerbose "Removendo diretório de arquivos temporários gerados pela ferramenta"
+    mensagemDebug "find "$CBUILD_TARGET_DIR/build" -delete"
+    find "$CBUILD_TARGET_DIR/build" -delete &&
+        mensagemVerbose "Diretório de arquivos temporários removido com sucesso" sucesso ||
+        imprimirMensagem "-Falha ao remover diretório de arquivos temporários" aviso sublinhado
 
-    mensagemVerbose "Projeto limpo com sucesso" sucesso
+    mensagemVerbose "Removendo arquivos de objeto intermediários gerados pelo compilador"
+    mensagemDebug "find "$CBUILD_TARGET_DIR/src" -type f -name "*.o" -delete"
+    find "$CBUILD_TARGET_DIR/src" -type f -name "*.o" -delete &&
+        mensagemVerbose "Arquivos de objeto intermediários removidos com sucesso" sucesso ||
+        imprimirMensagem "-Falha ao remover arquivos de objeto intermediários" aviso sublinhado
+
+    mensagemVerbose "Removendo arquivos de dependência intermediários gerados pelo compilador"
+    mensagemDebug "find "$CBUILD_TARGET_DIR/src" -type f -name "*.d" -delete"
+    find "$CBUILD_TARGET_DIR/src" -type f -name "*.d" -delete &&
+        mensagemVerbose "Arquivos de dependência intermediários removidos com sucesso" sucesso ||
+        imprimirMensagem "-Falha ao remover arquivos de dependência intermediários" aviso sublinhado
+
     mensagemComando "Projeto limpo com sucesso" sucesso 1
 }
 
