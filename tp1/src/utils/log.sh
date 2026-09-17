055847b054b23f945814692fe46cdd3b949ff2d8
14g0 <iagocesarts@gmail.com>
2026-09-17 14:46:05 -0300

    fix: adiciona verificações de permissão de escrita para o diretório de logs

--- tp1/src/utils/log.sh
+++ tp1/src/utils/log.sh
@@ -19,29 +19,44 @@ logComando() {
         'BEGIN { printf "%.6f", fim - inicio }'
     )
 
+    [[ ! -d "$CBUILD_TARGET_DIR" ||
+        ! -x "$CBUILD_TARGET_DIR" ||
+        ! -w "$CBUILD_TARGET_DIR"
+    ]] && saidaDeErro 400 "Sem permissão para criar o diretório de logs em $CBUILD_TARGET_DIR."
+
+    [[ -d "$CBUILD_TARGET_DIR/logs" &&
+      (! -w "$CBUILD_TARGET_DIR/logs" ||
+       ! -x "$CBUILD_TARGET_DIR/logs")
+    ]] && saidaDeErro 401 "Sem permissão para gravar em '$CBUILD_TARGET_DIR/logs'"
+
     mensagemVerbose "Verificando existência do diretório de logs em '$CBUILD_TARGET_DIR/logs'"
     mensagemDebug "[[ ! -d $CBUILD_TARGET_DIR/logs ]]"
     if [[ ! -d $CBUILD_TARGET_DIR/logs ]];
         then 
             mensagemVerbose "Criando diretório de logs em '$CBUILD_TARGET_DIR/logs'"
-            mensagemDebug "mkdir -p "$CBUILD_TARGET_DIR/logs""
+            mensagemDebug "mkdir -p $CBUILD_TARGET_DIR/logs"
 
             if mkdir -p "$CBUILD_TARGET_DIR/logs";
                 then mensagemVerbose "Diretório de logs criado com sucesso em '$CBUILD_TARGET_DIR/logs'" sucesso
-                else saidaDeErro 401 "Falha ao criar diretório de logs em '$CBUILD_TARGET_DIR/logs'"
+                else saidaDeErro 402 "Falha ao criar diretório de logs em '$CBUILD_TARGET_DIR/logs'"
             fi
     fi
     
     mensagemVerbose "Verificando existência do arquivo de logs em '$CBUILD_TARGET_DIR/logs/cbuild.log'"
     mensagemDebug "[[ ! -f $CBUILD_TARGET_DIR/logs/cbuild.log ]]"
-    if [[ ! -f $CBUILD_TARGET_DIR/logs/cbuild.log ]];
+    if [[ ! -f "$CBUILD_TARGET_DIR/logs/cbuild.log" ]];
         then
+            [[ -f "$CBUILD_TARGET_DIR/logs/cbuild.log" &&
+              ! -w "$CBUILD_TARGET_DIR/logs/cbuild.log"
+            ]] && saidaDeErro "Sem permissão para escrever em '$CBUILD_TARGET_DIR/logs/cbuild.log'"
+
             mensagemVerbose "Criando arquivo de logs em '$CBUILD_TARGET_DIR/logs/cbuild.log'"
-            mensagemDebug "touch "$CBUILD_TARGET_DIR/logs/cbuild.log""
+            mensagemDebug "touch $CBUILD_TARGET_DIR/logs/cbuild.log"
+
 
             if touch "$CBUILD_TARGET_DIR/logs/cbuild.log";
                 then mensagemVerbose "Arquivo de logs criado com sucesso em '$CBUILD_TARGET_DIR/logs/cbuild.log'" sucesso
-                else saidaDeErro 402 "Falha ao criar arquivo de logs em '$CBUILD_TARGET_DIR/logs/cbuild.log'"
+                else saidaDeErro 403 "Falha ao criar arquivo de logs em '$CBUILD_TARGET_DIR/logs/cbuild.log'"
             fi
     fi
 
@@ -53,7 +68,7 @@ logComando() {
     done
 
     mensagemVerbose "Registrando log do comando em '$CBUILD_TARGET_DIR/logs/cbuild.log'"
-    mensagemDebug "printf '%s|%s|%s|%s|%s\n' "$dataAtual" "$comando" "$tempo" "$CBUILD_LOG_CODE" "$flags""
+    mensagemDebug "printf '%s|%s|%s|%s|%s\n' $dataAtual $CBUILD_LOG_COMMAND $tempoExecucao $CBUILD_LOG_CODE ${flags[*]}"
     {
         printf '%s|%s|%s|%s|%s\n' "$dataAtual" "$tempoExecucao" "$CBUILD_LOG_COMMAND" "$CBUILD_LOG_CODE" "${flags[*]}"
         if [ -n "$CBUILD_LOG_MESSAGE" ]; then
@@ -66,21 +81,10 @@ logComando() {
 
 #-------------------------------------------------------------------------------
 
-verificarEstruturaDiretorios() {
-    if [[
-        ! -d "$CBUILD_TARGET_DIR/src" &&
-        ! -d "$CBUILD_TARGET_DIR/include"
-    ]];
-            then exit 400
-    fi
-}
-
-#-------------------------------------------------------------------------------
-
 registrarLog() {
-    verificarEstruturaDiretorios
-
     local status="$?"
+
+    [[ ! -d "$CBUILD_TARGET_DIR/src" && ! -d "$CBUILD_TARGET_DIR/include" ]] && return 1
 
     [[ "$CBUILD_END_TIME" == 0 ]] && CBUILD_END_TIME="${EPOCHREALTIME/,/.}"
 
